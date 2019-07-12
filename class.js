534d971dac051161247b652cc68ea44d09da5838
zxxzzzzz <812603872@qq.com>
2019-07-12 10:43:50 +0800

    Create class.js

---
+++ class.js
@@ -0,0 +1,22 @@
+// 一个最基本得类
+class MyO {
+    constructor(){
+
+    }
+}
+
+// 一个类继承
+class MyArray extends Array{
+    constructor(){
+        super() //必须super后才能用this
+        this.a = 1 //属性
+    }
+    //静态函数
+    static te(){
+
+    }
+    //方法
+    te2(){
+
+    }
+}
